a8700a3c0991ed4450d91362961b108ded839f3e
Beatris <beatrisraj7@gmail.com>
2025-03-09 19:20:17 +0200

    Attach context bridge to window

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,11 +1,15 @@
 import {app, BrowserWindow} from 'electron';
 import path from'path'
 import { isDev } from './utils.js';
+import { getPreloadPath } from './pathResolver.js';
 
 
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow()
-    // !! USE PATH BECOUSE WINDOWS USES \
+    const mainWindow = new BrowserWindow({
+        webPreferences:{
+            preload: getPreloadPath()
+        }
+    })
     if(isDev()){
         mainWindow.loadURL('http://localhost:5123')
     } else{
