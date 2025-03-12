8b38c2e6a520d31088cc7e2d4b206df4097cd54e
Beatris <beatrisraj7@gmail.com>
2025-03-12 16:48:13 +0200

    Create and Add Tasks

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,9 +1,23 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
+import TaskItem from './components/TaskItem'
+
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [value, setValue] = useState('')
+
+  function handleChange(e){
+    setValue(e.currentTarget.value)
+  }
+
+  function createTask(){
+    const id = uuid()
+    const newTask = {id, text:value}
+    setTasks(t => [...t, newTask])
+    setValue('')
+  }
   return (
     <>
   <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-16">
@@ -16,8 +30,11 @@ function App() {
           className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="text"
           placeholder="Add a task"
+          value={value}
+          onChange={handleChange}
         />
         <button
+          onClick={createTask}
           className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
           type="button"
         >
@@ -26,37 +43,10 @@ function App() {
       </div>
     </form>
     <ul className="divide-y divide-gray-200 px-4">
+      {tasks.map(currentTask => <TaskItem key={currentTask.id} data={currentTask} />)}
+
       
-      <li className="py-4">
-        <div className="flex items-center">
-          <input
-            id="todo2"
-            name="todo2"
-            type="checkbox"
-            className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
-          />
-          <label htmlFor="todo2" className="ml-3 block text-gray-900">
-            <span className="text-lg font-medium">Buy groceries</span>
-            <span className="text-sm font-light text-gray-500">
-              Bananas, milk, bread
-            </span>
-          </label>
-        </div>
-      </li>
-      <li className="py-4">
-        <div className="flex items-center">
-          <input
-            id="todo3"
-            name="todo3"
-            type="checkbox"
-            className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
-          />
-          <label htmlFor="todo3" className="ml-3 block text-gray-900">
-            <span className="text-lg font-medium">Go for a run</span>
-            <span className="text-sm font-light text-gray-500">3 miles</span>
-          </label>
-        </div>
-      </li>
+      
     </ul>
   </div>
 </>
--- src/ui/components/TaskItem.jsx
+++ src/ui/components/TaskItem.jsx
@@ -1,4 +1,4 @@
-export default function TaskItem() {
+export default function TaskItem({data}) {
     return (
         <li className="py-4">
             <div className="flex items-center">
@@ -9,10 +9,8 @@ export default function TaskItem() {
                     className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
                 />
                 <label htmlFor="todo1" className="ml-3 block text-gray-900">
-                    <span className="text-lg font-medium">Finish project proposal</span>
-                    <span className="text-sm font-light text-gray-500">
-                        Due on 4/1/23
-                    </span>
+                    <span className="text-lg font-medium">{data.text}</span>
+                    
                 </label>
             </div>
         </li>
